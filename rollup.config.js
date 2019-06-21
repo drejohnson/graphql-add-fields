440debb7d2f1d6795bc7aafb7b59a1318e17bc3b
DeAndre Johnson <dre@phreshr.com>
2019-06-22 00:20:49 +0400

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,47 @@
-const babel = require('rollup-plugin-babel')
-const { sizeSnapshot } = require('rollup-plugin-size-snapshot')
-const pkg = require('./package.json')
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import pkg from './package.json'
 
-const input = './index.js'
-const globals = { graphql: 'GraphQL' }
 const babelOpts = {
   exclude: '**/node_modules/**',
   runtimeHelpers: true
 }
 
-module.exports = {
-  input,
-  output: {
-    file: pkg.main,
-    format: 'cjs',
-    globals
-  },
-  treeshake: true,
-  external: Object.keys(globals),
-  plugins: [babel(babelOpts), sizeSnapshot()]
-}
+export default [
+  {
+    input: 'index.js',
+    external: Object.keys(pkg.dependencies).concat(
+      require('module').builtinModules ||
+        Object.keys(process.binding('natives'))
+    ),
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true
+      },
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      babel(babelOpts),
+      terser({
+        module: true
+      }),
+      sizeSnapshot()
+    ],
+    treeshake: true
+  }
+]
